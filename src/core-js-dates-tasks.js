d6f69020dcf7bc2f370efa6914740156d6a81867
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-12-19 10:39:20 +0500

    feat(tasks): implement getCountDaysInMonth function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -96,8 +96,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const lastDayOfMonth = new Date(year, month, 0);
+  return lastDayOfMonth.getDate();
 }
 
 /**
