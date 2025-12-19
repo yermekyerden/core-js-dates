9b824ab4d1b589aed7ada9e44ed31f215d5f57a4
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-12-19 11:40:54 +0500

    feat(tasks): implement isDateInPeriod function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -140,8 +140,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateMs = Date.parse(date);
+  const startMs = Date.parse(period.start);
+  const endMs = Date.parse(period.end);
+
+  return startMs <= dateMs && dateMs <= endMs;
 }
 
 /**
