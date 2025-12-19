c00c5c0dab14ab15c0effe9b79244a1ceae7dd87
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-12-19 11:38:11 +0500

    feat(tasks): implement getCountDaysOnPeriod function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -112,8 +112,15 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startMs = Date.parse(dateStart);
+  const endMs = Date.parse(dateEnd);
+
+  const MS_PER_DAY = 24 * 60 * 60 * 1000;
+
+  const diffDays = (endMs - startMs) / MS_PER_DAY;
+
+  return Math.round(diffDays) + 1;
 }
 
 /**
